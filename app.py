623dc25c1aed08e2f705efe198948401143bc022
tbenst <tbenst@gmail.com>
2016-07-05 15:59:13 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import os
+import fnmatch
+
+app = Flask(__name__)
+
+def voltages_file_generator(directory):
+    """Search directory for .voltages files and yield one-by-one."""
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, '*.voltages'):
+            filepath = os.path.join(root, filename)
+            if os.path.isfile(filepath):
+                yield filepath
+
+files = voltages_file_generator('/data')
+
+
+@app.route('/spyking/job')
+def spyking_job():
+    global files
+    next_voltage_file = None
+    try:
+        next_voltage_file = next(files)
+    except StopIteration:
+        try:
+            files = voltages_file_generator('/data')
+            next_voltage_file = next(files)
+        except:
+            return 'No new jobs.'
+
+
+    return create_return_tuple(next_voltage_file)
+
+def create_return_tuple(next_voltage_file):
+    name, ext = os.path.splitext(next_voltage_file)
+
+    return '("{}", "{}")'.format(next_voltage_file, name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
